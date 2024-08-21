a66852fe3f68d38563f1dc137e837d1600d34148
rafael.carvacho.p <rafael.carvacho.p@gmail.com>
2024-08-21 18:58:16 +0000

    refactor: :zap: separate 2 fetchings

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -3,18 +3,39 @@ import axios from 'axios'
 const MAX_RETRIES = 3
 const RETRY_DELAY = 5000
 
-export const fetchWithRetry = async (url: string, delay: number = RETRY_DELAY, retries = 0): Promise<string> => {
+export const fetchPage = async (url: string, delay: number = RETRY_DELAY, retries = 0): Promise<any | undefined> => {
   try {
     const { data } = await axios.get(url)
     return data
-  } catch (error) {
+  } catch (error: any) {
     if (retries < MAX_RETRIES) {
       console.log(`Retry ${retries + 1} for URL: ${url}`)
       await sleep(delay)
-      return await fetchWithRetry(url, retries + 1)
+      return await fetchPage(url, retries + 1)
     } else {
-      // throw error
-      throw new Error(`Failed to fetch URL: ${url} after ${MAX_RETRIES} retries.`)
+      console.error(`Failed to fetch URL: ${url} after ${MAX_RETRIES} retries.`)
+      return undefined
+    }
+  }
+}
+
+export const fetchPaper = async (url: string, delay: number = RETRY_DELAY, retries = 0): Promise<any | undefined> => {
+  try {
+    const { data } = await axios.get(url)
+    return data
+  } catch (error: any) {
+    if (error?.response?.status === 404) {
+      console.error(`URL not found: ${url}`)
+      return undefined
+    }
+
+    if (retries < MAX_RETRIES) {
+      console.log(`Retry ${retries + 1} for URL: ${url}`)
+      await sleep(delay)
+      return await fetchPage(url, retries + 1)
+    } else {
+      console.error(`Failed to fetch URL: ${url} after ${MAX_RETRIES} retries.`)
+      return undefined
     }
   }
 }
